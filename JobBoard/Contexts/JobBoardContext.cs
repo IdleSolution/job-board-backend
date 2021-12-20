0b181a48dea945dac9be52143ce4815ccfdf611d
Paweł Knapczyk <Pawlak.lis@gmail.com>
2021-12-20 12:34:20 +0100

    Add more companies data

--- JobBoard/Contexts/JobBoardContext.cs
+++ JobBoard/Contexts/JobBoardContext.cs
@@ -19,15 +19,25 @@ namespace JobBoard.Contexts
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Company>().HasData(
-                new { Id = 1L, Name = "Qualtrics" }
-                );
+                new { Id = 1L, Name = "Qualtrics" },
+                new { Id = 2L, Name = "Comarch"},
+                new { Id = 3L, Name = "SWM"},
+                new { Id = 4L, Name = "Capgemini"}
+
+            );
 
             modelBuilder.Entity<Review>().HasData(
-                new { Id = 1L, CompanyId = 1L, Rating = 5, Position = "Intern", Comment = "Jest niezle", Tag = "JavaScript", Issued = new System.DateTime(2021, 9, 1) }
-                );
+                new { Id = 1L, CompanyId = 1L, Rating = 5, Position = "Intern", Comment = "Jest niezle", Tag = "JavaScript", Issued = new System.DateTime(2021, 9, 1) },
+                new { Id = 2L, CompanyId = 2L, Rating = 1, Position = "Intern", Comment = "Jest niezle", Tag = "C#", Issued = new System.DateTime(2021, 9, 1) },
+                new { Id = 3L, CompanyId = 3L, Rating = 5, Position = "Intern", Comment = "Jest niezle", Tag = "Java", Issued = new System.DateTime(2021, 9, 1) },
+                new { Id = 4L, CompanyId = 4L, Rating = 5, Position = "Intern", Comment = "Jest niezle", Tag = "Java", Issued = new System.DateTime(2021, 9, 1) }
+            );
             modelBuilder.Entity<Interview>().HasData(
-                new { Id = 1L, CompanyId = 1L, Difficulty = 5, Position = "Intern", Comment = "Kinda hard", Tag = "JavaScript", Issued = new System.DateTime(2021, 9, 1) }
-                );
+                new { Id = 1L, CompanyId = 1L, Difficulty = 5, Position = "Intern", Comment = "Kinda hard", Tag = "JavaScript", Issued = new System.DateTime(2021, 9, 1) },
+                new { Id = 2L, CompanyId = 2L, Difficulty = 1, Position = "Intern", Comment = "ez", Tag = "C#", Issued = new System.DateTime(2021, 9, 1) },
+                new { Id = 3L, CompanyId = 3L, Difficulty = 1, Position = "Intern", Comment = "ez", Tag = "C#", Issued = new System.DateTime(2021, 9, 1) },
+                new { Id = 4L, CompanyId = 4L, Difficulty = 1, Position = "Intern", Comment = "ez", Tag = "C#", Issued = new System.DateTime(2021, 9, 1) }
+            );
         }
     }
 }
---
+++ JobBoard/Migrations/20211220112601_More.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JobBoard.Migrations
+{
+    public partial class More : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2L, "Comarch" });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 3L, "SWM" });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 4L, "Capgemini" });
+
+            migrationBuilder.InsertData(
+                table: "Interviews",
+                columns: new[] { "Id", "Comment", "CompanyId", "Difficulty", "Issued", "Position", "Tag" },
+                values: new object[,]
+                {
+                    { 2L, "ez", 2L, 1, new DateTime(2021, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Intern", "C#" },
+                    { 3L, "ez", 3L, 1, new DateTime(2021, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Intern", "C#" },
+                    { 4L, "ez", 4L, 1, new DateTime(2021, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Intern", "C#" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Reviews",
+                columns: new[] { "Id", "Comment", "CompanyId", "From", "Issued", "Position", "Rating", "Tag", "To" },
+                values: new object[,]
+                {
+                    { 2L, "Jest niezle", 2L, null, new DateTime(2021, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Intern", 1, "C#", null },
+                    { 3L, "Jest niezle", 3L, null, new DateTime(2021, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Intern", 5, "Java", null },
+                    { 4L, "Jest niezle", 4L, null, new DateTime(2021, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Intern", 5, "Java", null }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Interviews",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Interviews",
+                keyColumn: "Id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "Interviews",
+                keyColumn: "Id",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 4L);
+        }
+    }
+}
